43bd6bc64f02cde8795f8797eadbab3db46b7097
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-08-14 10:20:02 -0400

    Add project files.

---
+++ test/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace test
+{
+    public class Person
+    {
+        private static int id = 1;
+        public int ID { get; private set;}
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            ID = id;
+            id++;
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace test
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Person> test = new()
+            {
+                new("tom",31),
+                new("dave",45),
+                new("dick",51)
+            };
+
+            int slection = 1;
+
+            do
+            {
+                int index = 1;
+                Console.Clear();
+                foreach(Person item in test)
+                {
+                    if(index == slection)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                    }
+                    Console.WriteLine($"{item.ID}: {item.Name}");
+                    Console.ResetColor();
+                    index++;
+                }
+
+                Console.WriteLine("Key");
+                ConsoleKey key = Console.ReadKey().Key;
+                switch(key)
+                {
+                    case ConsoleKey.DownArrow:
+                        if(slection < test.Count)
+                        {
+                            slection++;
+                            break;
+                        }
+                        slection = 1;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if(slection > 1)
+                        {
+                            slection--;
+                            break;
+                        }
+                        slection = test.Count;
+                        break;
+                    case ConsoleKey.Enter:
+                        Person person = test.ElementAt(slection - 1);
+                        Console.Clear();
+                        Console.WriteLine($"{person.Name} is {person.Age} years old.");
+                        Console.WriteLine("Press any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        break;
+                }
+            } while(true);
+        }
+    }
+}
