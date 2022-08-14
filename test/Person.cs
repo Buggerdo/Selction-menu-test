77bf597911f3ccf75d1da4a9007687afc95eef1c
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-08-14 15:42:16 -0400

    more

--- test/Person.cs
+++ test/Person.cs
@@ -5,10 +5,12 @@
         private static int id = 1;
         public int ID { get; private set;}
         public string Name { get; set; }
+        public string LastName { get; set; }
         public int Age { get; set; }
-        public Person(string name, int age)
+        public Person(string name,string lastName, int age)
         {
             Name = name;
+            LastName = lastName;
             Age = age;
             ID = id;
             id++;
--- test/Program.cs
+++ test/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace test
+﻿using System.Text;
+
+namespace test
 {
     internal class Program
     {
@@ -6,34 +8,84 @@
         {
             List<Person> test = new()
             {
-                new("tom",31),
-                new("dave",45),
-                new("dick",51)
+                new("tom","suck",31),
+                new("dave","bob",45),
+                new("bob","dave",45),
+                new("steve","salfjn",57),
+                new("henry","ddieasln",67),
+                new("nick","henryes",12),
+                new("troy","wilson",23),
             };
 
+            List<Person> filteredPeople = new();
+
             int slection = 1;
+            int sort = 1;
+            //string search = "test";
+            StringBuilder search = new();
 
             do
             {
                 int index = 1;
+                filteredPeople.Clear();
                 Console.Clear();
-                foreach(Person item in test)
+                foreach(Person person in test)
+                {
+                    filteredPeople.Add(person);
+                }
+
+                if(sort == 1)
+                {
+                    filteredPeople = filteredPeople.OrderBy(x => x.Name).ToList();
+                    Console.WriteLine("Name");
+                }
+                else if(sort == 2)
+                {
+                    filteredPeople = filteredPeople.OrderBy(x => x.Age).ToList();
+                    Console.WriteLine("Age");
+                }
+
+                if(search.Length > 0)
+                {
+                    filteredPeople = filteredPeople.Where(x => x.Name.Contains(search.ToString(), StringComparison.OrdinalIgnoreCase) 
+                    || x.LastName.Contains(search.ToString(), StringComparison.OrdinalIgnoreCase)).ToList();
+                }
+            
+                foreach(Person item in filteredPeople)
                 {
                     if(index == slection)
                     {
                         Console.BackgroundColor = ConsoleColor.Red;
                     }
-                    Console.WriteLine($"{item.ID}: {item.Name}");
+                    Console.WriteLine($"{item.Name} {item.LastName} {item.Age}");
                     Console.ResetColor();
                     index++;
                 }
 
-                Console.WriteLine("Key");
+                Console.WriteLine(search);
+
+                if(search.ToString().ToLower() == "exit")
+                {
+                    Console.WriteLine("thank you for playing.");
+                    Console.WriteLine("press any key to continue.");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+
                 ConsoleKey key = Console.ReadKey().Key;
+
+                //if char add to search
+                if(char.IsLetter((char)key) || char.IsDigit((char)key))
+                {
+                    search.Append((char)key);
+                }
+
+    
+
                 switch(key)
                 {
                     case ConsoleKey.DownArrow:
-                        if(slection < test.Count)
+                        if(slection < filteredPeople.Count)
                         {
                             slection++;
                             break;
@@ -46,14 +98,36 @@
                             slection--;
                             break;
                         }
-                        slection = test.Count;
+                        slection = filteredPeople.Count;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if(sort < 2)
+                        {
+                            sort++;
+                            break;
+                        }
+                        sort = 1;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if(sort > 1)
+                        {
+                            sort--;
+                            break;
+                        }
+                        sort = 2;
                         break;
                     case ConsoleKey.Enter:
-                        Person person = test.ElementAt(slection - 1);
+                        Person person = filteredPeople.ElementAt(slection - 1);
                         Console.Clear();
                         Console.WriteLine($"{person.Name} is {person.Age} years old.");
                         Console.WriteLine("Press any key to continue.");
                         Console.ReadKey();
+                        break;
+                    case ConsoleKey.Backspace:
+                        if(search.Length > 0)
+                        {
+                            search.Remove(search.Length -1 ,1);
+                        }
                         break;
                     default:
                         break;
