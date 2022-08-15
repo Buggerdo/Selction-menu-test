052366ae4d2de050ed11ac19c66ae6171d2318d5
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-08-15 18:32:02 -0400

    Added menu items to the list of choices

--- test/Program.cs
+++ test/Program.cs
@@ -8,13 +8,20 @@ namespace test
         {
             List<Person> test = new()
             {
-                new("tom","suck",31),
-                new("dave","bob",45),
-                new("bob","dave",45),
-                new("steve","salfjn",57),
+                new("tom","cluck",25),
+                new("Dave","bob",43),
+                new("bob","Dave",45),
+                new("Steve","salfjn",57),
                 new("henry","ddieasln",67),
-                new("nick","henryes",12),
-                new("troy","wilson",23),
+                new("nick","Henry",12),
+                new("troy","Wilson",23),
+                new("tom","shuck",31),
+                new("Dave","bob",45),
+                new("bob","Dave",45),
+                new("Steve","salfjn",57),
+                new("henry","ddieasln",67),
+                new("nick","hen-ryes",12),
+                new("troy","Wilson",23),
             };
 
             List<Person> filteredPeople = new();
@@ -29,28 +36,39 @@ namespace test
                 int index = 1;
                 filteredPeople.Clear();
                 Console.Clear();
+                
+                //Adds items from list to filtered list
                 foreach(Person person in test)
                 {
                     filteredPeople.Add(person);
                 }
 
+
+                // Chooses the sort type 
                 if(sort == 1)
                 {
                     filteredPeople = filteredPeople.OrderBy(x => x.Name).ToList();
                     Console.WriteLine("Name");
                 }
-                else if(sort == 2)
+                if(sort == 2)
+                {
+                    filteredPeople = filteredPeople.OrderBy(x => x.LastName).ToList();
+                    Console.WriteLine("Last Name");
+                }
+                else if(sort == 3)
                 {
                     filteredPeople = filteredPeople.OrderBy(x => x.Age).ToList();
                     Console.WriteLine("Age");
                 }
 
+                // Applies the search criteria
                 if(search.Length > 0)
                 {
-                    filteredPeople = filteredPeople.Where(x => x.Name.Contains(search.ToString(), StringComparison.OrdinalIgnoreCase) 
-                    || x.LastName.Contains(search.ToString(), StringComparison.OrdinalIgnoreCase)).ToList();
+                    filteredPeople = filteredPeople.Where(x => x.Name.Contains(search.ToString(), StringComparison.OrdinalIgnoreCase)
+                    || x.LastName.Contains(search.ToString(), StringComparison.OrdinalIgnoreCase)
+                    || x.Age.ToString().Contains(search.ToString(), StringComparison.OrdinalIgnoreCase)).ToList();
                 }
-            
+
                 foreach(Person item in filteredPeople)
                 {
                     if(index == slection)
@@ -61,31 +79,29 @@ namespace test
                     Console.ResetColor();
                     index++;
                 }
-
-                Console.WriteLine(search);
-
-                if(search.ToString().ToLower() == "exit")
+                if(slection == filteredPeople.Count + 1)
                 {
-                    Console.WriteLine("thank you for playing.");
-                    Console.WriteLine("press any key to continue.");
-                    Console.ReadKey();
-                    Environment.Exit(0);
+                    Console.BackgroundColor = ConsoleColor.Red;
                 }
+                Console.WriteLine();
+                Console.WriteLine("Exit");
+                Console.ResetColor();
+                Console.WriteLine(search);
 
                 ConsoleKey key = Console.ReadKey().Key;
 
                 //if char add to search
-                if(char.IsLetter((char)key) || char.IsDigit((char)key))
+                if(char.IsLetter((char)key) || char.IsDigit((char)key) || char.IsWhiteSpace((char)key))
                 {
                     search.Append((char)key);
                 }
 
-    
+
 
                 switch(key)
                 {
                     case ConsoleKey.DownArrow:
-                        if(slection < filteredPeople.Count)
+                        if(slection < filteredPeople.Count + 1)
                         {
                             slection++;
                             break;
@@ -98,10 +114,10 @@ namespace test
                             slection--;
                             break;
                         }
-                        slection = filteredPeople.Count;
+                        slection = filteredPeople.Count + 1;
                         break;
                     case ConsoleKey.RightArrow:
-                        if(sort < 2)
+                        if(sort < 3)
                         {
                             sort++;
                             break;
@@ -114,20 +130,29 @@ namespace test
                             sort--;
                             break;
                         }
-                        sort = 2;
+                        sort = 3;
+                        break;     
+                    case ConsoleKey.Backspace:
+                        if(search.Length > 0)
+                        {
+                            search.Remove(search.Length - 1, 1);
+                        }
                         break;
                     case ConsoleKey.Enter:
+                        if(slection == filteredPeople.Count + 1)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("thank you for playing.");
+                            Console.WriteLine("press any key to continue.");
+                            Console.ReadKey();
+                            Environment.Exit(0);
+                        }
                         Person person = filteredPeople.ElementAt(slection - 1);
                         Console.Clear();
                         Console.WriteLine($"{person.Name} is {person.Age} years old.");
                         Console.WriteLine("Press any key to continue.");
                         Console.ReadKey();
-                        break;
-                    case ConsoleKey.Backspace:
-                        if(search.Length > 0)
-                        {
-                            search.Remove(search.Length -1 ,1);
-                        }
+                        search.Clear();
                         break;
                     default:
                         break;
